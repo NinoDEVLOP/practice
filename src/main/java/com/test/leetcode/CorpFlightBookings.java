8559d9e497f19e09ad15146b2fea0785274f0775
qingchuan.xia <q315xia@163.com>
2020-01-16 11:53:54 +0800

    blablabla

--- src/main/java/com/test/leetcode/CorpFlightBookings.java
+++ src/main/java/com/test/leetcode/CorpFlightBookings.java
@@ -19,19 +19,6 @@ public class CorpFlightBookings {
                 }
             }
 
-            // 3ms
-//	        int[] counters = new int[n];
-//	        for (int[] booking : bookings) {
-//	            counters[booking[0] - 1] += booking[2];
-//	            if (booking[1] < n) {
-//	                counters[booking[1]] -= booking[2];
-//	            }
-//	        }
-//	        for (int i = 1; i < n; ++i) {
-//	            counters[i] += counters[i - 1];
-//	        }
-//	        return counters;
-
             // n * c
 //	        for (int i = 0; i < result.length; i++) {
 //	            for (int[] inner : bookings) {
@@ -65,20 +52,6 @@ public class CorpFlightBookings {
     }
 
     static class ModifySolutionAfterViewAnswer {
-
-        public int[] corpFlightBookings(int[][] bookings, int n) {
-            int[] result = new int[n];
-            for (int[] booking : bookings) {
-                result[booking[0] - 1] += booking[2];
-                if (booking[1] != n) {
-                    result[booking[1]] -= booking[2];
-                }
-            }
-            for (int i = 1; i < n; i++) {
-                result[i] += result[i - 1];
-            }
-            return result;
-        }
 
     }
 
